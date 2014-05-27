ccf5e636803a55d062e97aaf4e2c27d5c787aff0
Mirza Kapetanovic <mirza.kapetanovic@gmail.com>
2014-05-27 13:26:21 +0200

    Added options.replacer for custom object serialization

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ module.exports = function (obj, opts) {
     var space = opts.space || '';
     if (typeof space === 'number') space = Array(space+1).join(' ');
     var cycles = (typeof opts.cycles === 'boolean') ? opts.cycles : false;
+    var replacer = opts.replacer || function(key, value) { return value; };
 
     var cmp = opts.cmp && (function (f) {
         return function (node) {
@@ -18,12 +19,18 @@ module.exports = function (obj, opts) {
     })(opts.cmp);
 
     var seen = [];
-    return (function stringify (node, level) {
+    return (function stringify (parent, key, node, level) {
         var indent = space ? ('\n' + new Array(level + 1).join(space)) : '';
         var colonSeparator = space ? ': ' : ':';
 
         if (node && node.toJSON && typeof node.toJSON === 'function') {
             node = node.toJSON();
+        }
+
+        node = replacer.call(parent, key, node);
+
+        if (node === undefined) {
+            return;
         }
         if (typeof node !== 'object' || node === null) {
             return json.stringify(node);
@@ -31,14 +38,14 @@ module.exports = function (obj, opts) {
         if (isArray(node)) {
             var out = [];
             for (var i = 0; i < node.length; i++) {
-                var item = stringify(node[i], level+1);
+                var item = stringify(node, i, node[i], level+1) || json.stringify(null);
                 out.push(indent + space + item);
             }
             return '[' + out.join(',') + indent + ']';
         }
         else {
             if (seen.indexOf(node) !== -1) {
-                if (cycles) return stringify('__cycle__');
+                if (cycles) return json.stringify('__cycle__');
                 throw new TypeError('Converting circular structure to JSON');
             }
             else seen.push(node);
@@ -47,15 +54,19 @@ module.exports = function (obj, opts) {
             var out = [];
             for (var i = 0; i < keys.length; i++) {
                 var key = keys[i];
-                var keyValue = stringify(key,0)
+                var value = stringify(node, key, node[key], level+1);
+
+                if(!value) continue;
+
+                var keyValue = json.stringify(key)
                     + colonSeparator
-                    + stringify(node[key],level+1)
+                    + value;
                 ;
                 out.push(indent + space + keyValue);
             }
             return '{' + out.join(',') + indent + '}';
         }
-    })(obj, 0);
+    })({ '': obj }, '', obj, 0);
 };
 
 var isArray = Array.isArray || function (x) {
